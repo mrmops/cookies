238e52214c8a407ca6bb45e6efe6e99481a198b8
mrmops <30927821+mrmops@users.noreply.github.com>
2020-03-13 21:52:50 +0500

    Change Recipe class and add Comment.java

---
+++ src/main/java/com/newZcookies/cookies/Comment.java
@@ -0,0 +1,10 @@
+package com.newZcookies.cookies;
+
+import java.sql.Date;
+
+public class Comment {
+    private long Id;
+    private long AuthorId;
+    private Date TimeOfCreate;
+    private String Text;
+}
--- src/main/java/com/newZcookies/cookies/Recipe.java
+++ src/main/java/com/newZcookies/cookies/Recipe.java
@@ -1,5 +1,27 @@
 package com.newZcookies.cookies;
 
+import java.util.List;
+
 public class Recipe {
-    
+    private long Id;
+    private String Name;
+    private int Group;
+    private String Description;
+    private double Rating;
+    private long AuthorId;
+    private List<Comment> Comments;
+
+    public Recipe(long id, String name, int group, String description, double rating, long authorId, List<Comment> comments){
+        Id = id;
+        Name = name;
+        Group = group;
+        Description = description;
+        Rating = rating;
+        AuthorId = authorId;
+        Comments = comments;
+    }
+
+    public String GetName(){
+        return Name;
+    }
 }
