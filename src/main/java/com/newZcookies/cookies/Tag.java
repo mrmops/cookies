df09a61ac1b0ca4aa57902814e95a5d19491935c
Denis Matafonov <dmatafonov2000@mail.ru>
2020-04-02 08:11:54 +0500

    Add annotations to Tag

--- src/main/java/com/newZcookies/cookies/Tag.java
+++ src/main/java/com/newZcookies/cookies/Tag.java
@@ -1,9 +1,16 @@
 package com.newZcookies.cookies;
 
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 public class Tag {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
+
+    @Column(name="name")
     private String Name;
 }
