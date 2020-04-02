7fbedd226ca1b1e8d1dfc74ff06289aa932a45af
mrmops <30927821+mrmops@users.noreply.github.com>
2020-04-02 13:58:59 +0500

    User class

--- src/main/java/com/newZcookies/cookies/User.java
+++ src/main/java/com/newZcookies/cookies/User.java
@@ -1,4 +1,39 @@
 package com.newZcookies.cookies;
 
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "Users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+
+    @Column(name = "login")
+    private String Login;
+
+    @Column(name = "name")
+    private String Name;
+
+    @Column(name = "second_name")
+    private String SecondName;
+
+    @MapsId
+    @ManyToOne
+    @JoinColumn(name = "recipes")
+    private List<Recipe> Recipes;
+
+    public User(){
+
+    }
+
+    public User(String login, String name, String secondName, List<Recipe> recipes) {
+        Login = login;
+        Name = name;
+        SecondName = secondName;
+        Recipes = recipes;
+    }
 }
