818aa80a34264907c5366480ae16aac6c289ef4d
Gar1NN <gartas.red@yandex.ru>
2020-05-19 18:06:34 +0500

    Починил контроллер

--- src/main/java/com/newZcookies/cookies/Controllers/SearchController.java
+++ src/main/java/com/newZcookies/cookies/Controllers/SearchController.java
@@ -3,12 +3,14 @@ package com.newZcookies.cookies.Controllers;
 import com.newZcookies.cookies.Recipe;
 import com.newZcookies.cookies.servises.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+@Controller
 public class SearchController {
     @Autowired
     private RecipeService recipeService;
@@ -21,9 +23,4 @@ public class SearchController {
         return "searchPage";
     }
 
-    @RequestMapping("/login-error")
-    public String loginError(Model model) {
-        model.addAttribute("loginError", true);
-        return "login";
-    }
 }
